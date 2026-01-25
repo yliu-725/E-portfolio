9f9043c6a58940fa8c33448ed82e80c4eec48eda
Yihui <yliugbhr@gmail.com>
2026-01-24 23:15:59 -0500

    dark mode

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 // template id template_0lug9gd
 // service id service_xgpogz8
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+   contrastToggle = !contrastToggle;
+   if (contrastToggle) {
+    document.body.classList += " dark-theme"
+   }
+   else {
+    document.body.classList.remove("dark-theme")
+   }
+}
+
+function contact() {
+    console.log(work)
+}
+
+
+function toggleModal() {
+    document.body.classList += " modal--open"
+}
